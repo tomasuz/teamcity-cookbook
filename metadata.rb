b931d61e8fa11deca63b104fe6ef222b43d3ca77
Uldis Sturms <uldis.sturms@gmail.com>
2015-06-11 01:23:49 +0100

    Cleanup unused service password and add supported recipes and platforms to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license          'apache license version 2.0'
 description      'installs and configures teamcity'
 long_description 'installs and configures teamcity'
 version          '0.1.0'
+
+recipe 'teamcity', 'Installs Teamcity server'
+
+supports 'centos', '~> 7.0'
+
