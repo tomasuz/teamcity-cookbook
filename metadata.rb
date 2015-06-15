583ecf81e9d36e6a82ab5f345365c2066e447f93
Uldis Sturms <uldis.sturms@gmail.com>
2015-06-15 21:49:41 +0100

    Version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'teamcity-cookbook'
 maintainer       'uldis sturms'
 maintainer_email 'uldis.sturms@gmail.com'
 license          'apache license version 2.0'
-description      'installs and configures teamcity'
-long_description 'installs and configures teamcity'
-version          '0.1.1'
+description      'Installs and configures teamcity'
+long_description 'Installs and configures teamcity CI in a completely unattended manner. Test first approach using test kitchen is used to prevent regressions.'
+version          '0.1.2'
 
 recipe 'teamcity-cookbook', 'Installs Teamcity server'
 
