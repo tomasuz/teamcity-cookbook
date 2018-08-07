335181cea7730c8b77fba6eed2a32b2988ed8bb7
Tomas Martišius <tomas.martisius@getcujo.com>
2018-08-07 16:14:35 +0300

    Remove unused step.

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,12 @@ maintainer_email 'uldis.sturms@gmail.com'
 license          'apache license version 2.0'
 description      'Installs and configures teamcity'
 long_description 'Installs and configures teamcity CI in a completely unattended manner. Test first approach using test kitchen is used to prevent regressions.'
-version          '0.1.2'
+version          '0.1.3'
 
 recipe 'teamcity-cookbook', 'Installs Teamcity server'
 
 supports 'centos', '~> 7.0'
+supports 'ubuntu', '~> 16.0'
+supports 'debian', '~> 8.0'
 
 depends 'java'
