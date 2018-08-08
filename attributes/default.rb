fc8c08f2e7e0f59355fb0a747205fbbe1004d3cd
Tomas Martišius <tomas.martisius@getcujo.com>
2018-08-08 11:37:20 +0300

    OPS-520 Fix sysv init script.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,5 @@ default['teamcity']['accept-license'] = true
 default['teamcity']['admin-username'] = 'teamcity'
 default['teamcity']['admin-password'] = 'teamcity'
 default['java']['jdk_version'] = 8
+# possible :sysv and :systemd
+default['teamcity']['init_style'] = :systemd,
